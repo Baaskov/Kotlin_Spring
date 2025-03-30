237bfbfc3e85831f1dc4c2bee5b8544962873bcc
baaska <baaska.sotnicov@gmail.com>
2025-03-30 23:13:10 +0900

    KS-1-3 create smth

---
+++ src/main/kotlin/lesson_1/lesson1_4.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_1
+
+import java.util.Spliterator.OfDouble
+
+fun main() {
+    val lenght: String = "40_868_600_000"
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: UInt = 327000u
+
+    println(lenght)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
