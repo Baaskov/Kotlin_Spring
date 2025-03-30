1664db5ed1417e2f8aecdf67584966d474c0e63b
baaska <baaska.sotnicov@gmail.com>
2025-03-30 23:02:19 +0900

    KS-1-3 create smth

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main() {
+    val year: Int = 1961
+    println("Год взлета - " + year)
+
+    var hour: String = "09"
+    var minute: String = "07"
+    println("Взлет - " + hour + ":" + minute)
+
+    hour = "10"
+    minute = "55"
+    println("Посадка - " + hour + ":" + minute)
+
+}
