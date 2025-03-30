d614926e0af6a8f4ae65158ef7ce28072a80158e
baaska <baaska.sotnicov@gmail.com>
2025-03-31 00:25:08 +0900

    KS-2-1 create smth

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main() {
+    var student_1: Int= 3
+    var student_2: Int= 4
+    var student_3: Int= 3
+    var student_4: Int= 5
+
+    var avgScore: Float = (student_1 + student_2 + student_3 + student_4) / 4f
+
+    println(avgScore)
+}
