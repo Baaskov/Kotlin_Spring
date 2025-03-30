a8e816c4d34333974ed9073c53a55e51d0cc5cb6
baaska <baaska.sotnicov@gmail.com>
2025-03-30 22:52:01 +0900

    KS-1-2 create smth

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main() {
+    var order_counts: UInt = 75u
+    val text_order: String = "Благодорим за ваш заказ!"
+
+    println(order_counts)
+    println(text_order)
+
+    var workers: Int = 2000
+//    println(workers)
+
+    workers = 1999
+    println(workers)
+}
