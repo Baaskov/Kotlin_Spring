8f59c23415411b0404b42fbd55cbf596a1044b3b
baaska <baaska.sotnicov@gmail.com>
2025-03-31 21:10:00 +0900

    KS-2-4 create smth

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main() {
+    var buff: Int = 20
+    var crystalOre: Int = 7
+    var ironOre: Int = 11
+
+    println("Бонус кристалической руды: " + crystalOre*buff/100)
+    println("Бонус железной руды: " + ironOre*buff/100)
+}
