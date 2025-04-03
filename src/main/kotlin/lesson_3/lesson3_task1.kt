4e71701d7ab1eb90302405d5f4a833f78131a87a
baaska <baaska.sotnicov@gmail.com>
2025-04-03 22:33:00 +0900

    KS-3-1 adding a task

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_3
+
+fun main() {
+    val name: String = "Василий"
+    val greetingTextDay: String = "Добрый день"
+    val greetingTextNight: String = "Добрый вечер"
+
+    println("$greetingTextDay, $name.")
+    println("$greetingTextNight, $name.")
+}
